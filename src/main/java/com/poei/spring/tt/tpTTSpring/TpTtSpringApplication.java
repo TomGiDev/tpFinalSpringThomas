157988ecdc047d1894c391b9164f22611627d757
TomGiDev <thomas.girardet@orange.fr>
2022-06-08 14:48:58 +0200

    premier commit pour projet tp spring

---
+++ src/main/java/com/poei/spring/tt/tpTTSpring/TpTtSpringApplication.java
@@ -0,0 +1,13 @@
+package com.poei.spring.tt.tpTTSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTtSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poei/spring/tt/tpTTSpring/TpTtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poei.spring.tt.tpTTSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTtSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
