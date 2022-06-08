2f26d22e0a6c57dfe53988ee7f92546c6ea2f9c2
Raphaël <timothee.lerailler@epsi.fr>
2022-06-08 16:18:37 +0200

    script

--- src/main/resources/db.script/create_tables.sql
+++ src/main/resources/db.script/create_tables.sql
@@ -34,30 +34,30 @@ CREATE TABLE technicians
 );
 
 CREATE TABLE vehicules (
-    id        SERIAL PRIMARY KEY NOT NULL,
-    plate VARCHAR(7),
-    brand VARCHAR(200),
-    yearConstruction VARCHAR(4),
-    techinician_id INT,
-    FOREIGN KEY (techinician_id) REFERENCES technicians(id)
+                           id        SERIAL PRIMARY KEY NOT NULL,
+                           plate VARCHAR(7),
+                           brand VARCHAR(200),
+                           yearConstruction VARCHAR(4),
+                           techinician_id INT,
+                           FOREIGN KEY (techinician_id) REFERENCES technicians(id)
 );
 
 CREATE TABLE works (
-    id SERIAL PRIMARY KEY NOT NULL,
-    workName VARCHAR(255),
-    workPrice VARCHAR(255)
+                       id SERIAL PRIMARY KEY NOT NULL,
+                       workName VARCHAR(255),
+                       workPrice VARCHAR(255)
 );
 
 INSERT INTO adresses (streetNumber, road, city)
-VALUES ('1', 'Place de l\'eglise', 'Paris');
+VALUES (1, 'Place de l''eglise', 'Paris');
 INSERT INTO adresses (streetNumber, road, city)
-VALUES ('13', 'Place de la Mairie', 'Rennes');
+VALUES (13, 'Place de la Mairie', 'Rennes');
 INSERT INTO adresses (streetNumber, road, city)
-VALUES ('56', 'Chemin des Brumes', 'Brest');
+VALUES (56, 'Chemin des Brumes', 'Brest');
 INSERT INTO adresses (streetNumber, road, city)
-VALUES ('65', 'Rue des Platanes', 'Lannion');
+VALUES (65, 'Rue des Platanes', 'Lannion');
 INSERT INTO adresses (streetNumber, road, city)
-VALUES ('23', 'rue de la gare', 'Rennes');
+VALUES (23, 'rue de la gare', 'Rennes');
 
 INSERT INTO managers (firstname, lastname, phonenumber, mobilenumber)
 VALUES ('KENOBI', 'Obi-Wan', '0243456789', '0612345678');
@@ -67,40 +67,30 @@ INSERT INTO managers (firstname, lastname, phonenumber, mobilenumber)
 VALUES ('Vador', 'Dark', '0233456700', '0610005678');
 
 
+
 INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Trooper', '01', '24', 0, 0);
+VALUES ('Troper', '02', '34', 2, 2);
 INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Troper', '02', '34', 1, 1);
+VALUES ('Troopper', '03', '55', 3, 3);
 INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Troopper', '03', '55', 2, 2);
+VALUES ('Trouper', '04', '66', 3, 4);
 INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Trouper', '04', '66', 2, 0);
-INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Trop peur', '05', '23', 1, 1);
-INSERT INTO technicians (firstname, lastname, age, manager_id, adresse_id)
-VALUES ('Tropure', '06', '22', 0, 2);
+VALUES ('Trop peur', '05', '23', 1, 5);
 
 INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
-VALUES ('66TYU56', 'Opel', '1923', 0);
-INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
-VALUES ('66TYU22', 'Porche', '2345', 1);
+VALUES ('66TYU56', 'Opel', '1923', 1);
 INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
 VALUES ('66TYU11', 'BMW', '1234', 2);
 INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
 VALUES ('44TYU56', 'RENAULT', '4231', 3);
 INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
 VALUES ('55TUU56', 'DACIA', '1111', 4);
-INSERT INTO vehicules (plate, brand, yearConstruction, techinician_id)
-VALUES ('66TEE99', 'FERRARI', '2222', 5);
 
 INSERT INTO works (workName, workPrice)
-VALUES ('Eglise', '234567');
+VALUES ('Eglise', 234567);
 INSERT INTO works (workName, workPrice)
-VALUES ('Station', '345345');
+VALUES ('Station', 345345);
 INSERT INTO works (workName, workPrice)
-VALUES ('Ecole', '900000');
+VALUES ('Ecole', 900000);
 INSERT INTO works (workName, workPrice)
-VALUES ('Route', '1200000');
-
-
-
+VALUES ('Route', 1200000);
