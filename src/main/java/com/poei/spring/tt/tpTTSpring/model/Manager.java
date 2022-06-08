67210cfc6551d22a90f95df835d38d0cdc00f9cc
Raphaël <timothee.lerailler@epsi.fr>
2022-06-08 17:01:00 +0200

    Models

---
+++ src/main/java/com/poei/spring/tt/tpTTSpring/model/Manager.java
@@ -0,0 +1,35 @@
+package com.poei.spring.tt.tpTTSpring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "managers")
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    @Column(length = 10)
+    private String phonenumber;
+
+    @Column(length = 10)
+    private String mobilenumber;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Technician> technicians;
+}
