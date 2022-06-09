5d14549580648585c85cd85fda585726dbdd80d6
TomGiDev <thomas.girardet@orange.fr>
2022-06-09 10:59:02 +0200

    servicesImpl et exception

--- src/main/java/com/poei/spring/tt/tpTTSpring/service/impl/AddressServiceImpl.java
+++ src/main/java/com/poei/spring/tt/tpTTSpring/service/impl/AddressServiceImpl.java
@@ -1,4 +1,27 @@
 package com.poei.spring.tt.tpTTSpring.service.impl;
 
-public class AddressServiceImpl {
+import com.poei.spring.tt.tpTTSpring.exception.UnknownResourceException;
+import com.poei.spring.tt.tpTTSpring.model.Address;
+import com.poei.spring.tt.tpTTSpring.repository.AddressRepository;
+import com.poei.spring.tt.tpTTSpring.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Override
+    public List<Address> getAll() {return this.addressRepository.findAll(Sort.by("city").ascending());}
+
+    @Override
+    public Address getById(Integer id) {
+        return this.addressRepository.findById(id)
+                .orElseThrow(() -> new UnknownResourceException("No address found for this city"));
+    }
 }
