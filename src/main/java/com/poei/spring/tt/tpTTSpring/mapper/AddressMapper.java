3c29d65492566b0ba606e7bf9dd656b1b32c0ff1
Raphaël <timothee.lerailler@epsi.fr>
2022-06-09 13:35:17 +0200

    AddressMapping fixed

--- src/main/java/com/poei/spring/tt/tpTTSpring/mapper/AddressMapper.java
+++ src/main/java/com/poei/spring/tt/tpTTSpring/mapper/AddressMapper.java
@@ -3,6 +3,7 @@ package com.poei.spring.tt.tpTTSpring.mapper;
 import com.poei.spring.tt.tpTTSpring.api.dto.AddressDto;
 import com.poei.spring.tt.tpTTSpring.model.Address;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Component;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AddressMapper {
 
+    @Mapping(source = "technician.id", target = "technicianId")
     AddressDto mapToDto(Address address);
 
+    @Mapping(source = "technicianId", target = "technician.id")
     Address matToModel(AddressDto addressDto);
 }
