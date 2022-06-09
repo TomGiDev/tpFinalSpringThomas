ae5a54e9e19d3ab4952f01b7342c251ff19a2c93
Raphaël <timothee.lerailler@epsi.fr>
2022-06-09 13:38:58 +0200

    Api creation

--- src/main/java/com/poei/spring/tt/tpTTSpring/api/v1/WorkApi.java
+++ src/main/java/com/poei/spring/tt/tpTTSpring/api/v1/WorkApi.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/address")
-public class AddressApi {
-
-
+@RequestMapping("/v1/work")
+public class WorkApi {
 }
